2c22a3e13f6d8cb745427cfee281aa1e006dfedb
MukiBwoi <muksith20000309@gmail.com>
2022-02-08 12:40:25 +0530

    fe

--- lib/Screens/wrapper.dart
+++ lib/Screens/wrapper.dart
@@ -3,8 +3,7 @@ import 'package:brightfuture/Models/Providers/drawer_tile_change.dart';
 import 'package:brightfuture/Models/Providers/login_state.dart';
 import 'package:brightfuture/Models/screen_size.dart';
 import 'package:brightfuture/Screens/home.dart';
-import 'package:brightfuture/Widgets/Custom%20Button/custom_button.dart';
-import 'package:brightfuture/Widgets/CustomText/custom_text.dart';
+import 'package:brightfuture/Widgets/Error%20Page/error_page.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -31,56 +30,5 @@ class Wrapper extends StatelessWidget {
       return _isLoggedIn ? const Home() : const Login();
     }
     return ErrorPage(isLoggedIn: _isLoggedIn);
-  }
-}
-
-
-
-
-class ErrorPage extends StatelessWidget {
-  final bool isLoggedIn;
-  const ErrorPage({Key? key, required this.isLoggedIn}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final connection = Provider.of<CheckConnectivity>(context, listen: true);
-    return Scaffold(
-        body: SizedBox(
-      width: ScreenSize.width,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const CustomText(
-            title: "No Internet Connection",
-            fontWeight: FontWeight.bold,
-            fontSize: 25,
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          CustomButton(
-            label: "Try Again",
-            height: 50,
-            minWidth: ScreenSize.width * 0.5,
-            radius: 20,
-            onPressed: () {
-              connection.tryConnection();
-              if (connection.isConnectionSuccessful ||
-                  connection.connectivityResult != ConnectivityResult.none) {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (_) {
-                      return isLoggedIn ? const Home() : const Login();
-                    },
-                  ),
-                );
-              }
-            },
-          ),
-        ],
-      ),
-    ));
   }
 }
